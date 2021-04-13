ff9a0e7aa39dde70a9097425ae1943d1bcaebdcf
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-04-13 13:16:09 -0400

    nested reports in reportsla

--- lib/reportsla.rb
+++ lib/reportsla.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "reportsla/version"
-require_relative "reports/base"
+require_relative "reportsla/reports/base"
 
 module Reportsla
   class Error < StandardError; end
