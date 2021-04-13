167f4583192b9b819e436dd15e698f0a0de59c43
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-04-13 13:46:25 -0400

    added configuration to reportsla

--- lib/reportsla.rb
+++ lib/reportsla.rb
@@ -6,6 +6,23 @@ require_relative "reports/base"
 
 module Reportsla
   class Error < StandardError; end
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :registers
+    def initialize
+      @registers = {}
+    end
+
+  end
   # Your code goes here...
 
 end
